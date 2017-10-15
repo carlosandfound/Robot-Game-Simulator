523fc3c5d1c5b8410421d4581ea7e80593ee5d6c
Carlos <alvar357@umn.edu>
2017-10-15 13:06:13 -0500

    removing docs folder

--- project/iteration1/docs/html/search/all_c.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['sensor_5ftouch_2ecc',['sensor_touch.cc',['../sensor__touch_8cc.html',1,'']]],
-  ['sensor_5ftouch_2eh',['sensor_touch.h',['../sensor__touch_8h.html',1,'']]],
-  ['sensortouch',['SensorTouch',['../class_sensor_touch.html',1,'']]]
-];
