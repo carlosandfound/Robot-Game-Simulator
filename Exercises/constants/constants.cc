217bb5e7af3a777d978c0550d9f42aea0605e929
Carlos Alvarenga <alvar357@umn.edu>
2017-10-18 20:36:33 +0000

    adding contants ex

--- Exercises/constants/constants.cc
+++ Exercises/constants/constants.cc
@@ -6,19 +6,19 @@ const int myConst2 = 200;
 const int* pMyConst;
 
 // init integer
-//myConst1 = 100;
+// ERROR -- myConst1 = 100;
 
 // init pointer
-//pMyConst = &myConst2;
+pMyConst = &myConst2;
 
 // change pointer
-//pMyConst = &myConst2;
+pMyConst = &myConst2;
 
 // change integer
-//myConst2 = myConst2 + 5;
+// ERROR -- myConst2 = myConst2 + 5;
 
 // change integer using pointer
-//*pMyConst = myConst1 + 1;
+// ERROR -- *pMyConst = myConst1 + 1;
 
 int myInt_3;
 int myInt_4 = 400;
@@ -26,19 +26,19 @@ int* const cpMyInt_3 = &myInt_3;
 
 // initialize the pointer
 //int* const cpMyInt_4;
-//int* const cpMyInt_4 = &myInt_4;
+int* const cpMyInt_4 = &myInt_4;
 
 // initialize integer
-//myInt_3 = 300;
+myInt_3 = 300;
 
 // change the integer
-//myInt_4 = myInt_4 + 5;
+myInt_4 = myInt_4 + 5;
 
 // change integer using pointer
-//*cpMyInt_3 = myInt_4;
+*cpMyInt_3 = myInt_4;
 
 // change the pointer
-//cpMyInt_3 = &myInt_4;
+// ERROR - CONTANT POINTER CAN'T HAVE IT'S ADDRESS BE CHANGED cpMyInt_3 = &myInt_4;
 
 
 }
